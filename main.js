7c7e788a4fe80e59e1802c28416edfe24830a41f
Eugene Israel <90962682+K-Ochola@users.noreply.github.com>
2023-07-01 11:46:13 +0300

    Update main.js

--- main.js
+++ main.js
@@ -5,3 +5,16 @@ myImage.onclick = () => {
         myImage.setAttribute("src", "mdn-images.png/firefox2.png.jpg");
     }else{myImage.setAttribute("src", "mdn-images.png/firefox-image.png.jpg");}
 };
+let myButton = document.querySelector("button")
+let myHeading = document.querySelector("h1")
+function setUserName(){
+    const myName = prompt("Enter your user name");
+    localStorage.setItem("name", myName);
+    myHeading.textContent = 'Mozilla is cool, ${myName}';
+}
+if (!localStorage.getItem ("name")) {
+    setUserName();
+}else{
+    const storedName = localStorage.getItem("name");
+    myHeading.textContent = 'Mozilla is cool, ${storedName}';
+}
